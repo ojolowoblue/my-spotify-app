158733e11fece3be75d9c1bb402920e0e57b10a8
ojolowoblue <ojolowoblue@gmail.com>
2023-05-24 05:53:48 +0100

    rename

--- client/src/app/views/Home/index.tsx
+++ client/src/app/views/Home/index.tsx
@@ -1,13 +1,12 @@
 import useGetUser from "./hooks/useGetUser";
-import Loading from "app/components/AppLoader";
+import AppLoader from "app/components/AppLoader";
 
 export default function Home() {
   const { isLoading, isFetching } = useGetUser();
+
   return (
-    <div>
-      <Loading loading={isLoading || isFetching}>
-        <div className="text-white">The Home Page</div>
-      </Loading>
-    </div>
+    <AppLoader loading={isLoading || isFetching}>
+      <div className="text-white">The Home Page</div>
+    </AppLoader>
   );
 }
